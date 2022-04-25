38daa27fd913ab42e180d416dc6ade8f7c222bd0
dJacoboSanta <84555759+dJacoboSanta@users.noreply.github.com>
2022-04-24 22:08:22 -0500

    Second Practice

---
+++ Practice_Mona/practiceMona/Practice_Mona.java
@@ -0,0 +1,15 @@
+package practiceMona;
+
+public class Practice_Mona {
+    public static void main(String[] args){
+        Mona monaCat = new Mona();
+
+        monaCat.display("Yogitocat", "Yoga");
+        monaCat.display("Manufacturetocat", "Manufactura");
+        monaCat.display("Boxertocat", "Boxeador");
+        monaCat.display("Sentrytocat", "Bombero");
+        monaCat.display("Surftocat", "Surfista");
+        monaCat.display("Labtocat", "Cient\u00edfico");
+
+    }
+}
