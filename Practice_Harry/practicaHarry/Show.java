2f2cbae30c566fc3fd7374f7f3b08578560fb714
dJacoboSanta <84555759+dJacoboSanta@users.noreply.github.com>
2022-04-24 22:07:48 -0500

    First Practice

---
+++ Practice_Harry/practicaHarry/Show.java
@@ -0,0 +1,83 @@
+package practicaHarry;
+
+public class Show {
+    
+    public void showCharacter1(){
+        Character character = new Character();
+
+        character.setHouse("Gryffindor");
+        character.setGender("Male");
+        character.setName("Ron Weasley");
+        character.setBoggart("Spiders");
+        character.setPatronus("Jack Ruseell Terrier");
+
+        System.out.println("Name: " + character.getName());
+        System.out.println("Gender: " + character.getGender());
+        System.out.println("House: " + character.getHouse());
+        System.out.println("Boggart: " + character.getBoggart());
+        System.out.println("Patronus: " + character.getPatronus() + "\n");
+    }
+
+    public void showCharacter2(){
+        Character character = new Character();
+
+        character.setHouse("Gryffindor");
+        character.setGender("Male");
+        character.setName("Remus Lupin");
+        character.setBoggart("Full Moon");
+        character.setPatronus("Wolf");
+
+        System.out.println("Name: " + character.getName());
+        System.out.println("Gender: " + character.getGender());
+        System.out.println("House: " + character.getHouse());
+        System.out.println("Boggart: " + character.getBoggart());
+        System.out.println("Patronus: " + character.getPatronus() + "\n");
+    }
+
+    public void showCharacter3(){
+        Character character = new Character();
+
+        character.setHouse("Gryffindor");
+        character.setGender("Female");
+        character.setName("Hermione Granger");
+        character.setBoggart("Professor McGonagall tells her that she failed all his exams");
+        character.setPatronus("Otter");
+
+        System.out.println("Name: " + character.getName());
+        System.out.println("Gender: " + character.getGender());
+        System.out.println("House: " + character.getHouse());
+        System.out.println("Boggart: " + character.getBoggart());
+        System.out.println("Patronus: " + character.getPatronus() + "\n");
+    }
+
+    public void showCharacter4(){
+        Character character = new Character();
+
+        character.setHouse("Gryffindor");
+        character.setGender("Male");
+        character.setName("Harry Potter");
+        character.setBoggart("Dementor");
+        character.setPatronus("Deer");
+
+        System.out.println("Name: " + character.getName());
+        System.out.println("Gender: " + character.getGender());
+        System.out.println("House: " + character.getHouse());
+        System.out.println("Boggart: " + character.getBoggart());
+        System.out.println("Patronus: " + character.getPatronus() + "\n");
+    }
+    public void showCharacter5(){
+        Character character = new Character();
+
+        character.setHouse("Gryffindor");
+        character.setGender("Female");
+        character.setName("Ginny Weasley");
+        character.setBoggart("Be controlled again by Voldemort");
+        character.setPatronus("Horse");
+
+        System.out.println("Name: " + character.getName());
+        System.out.println("Gender: " + character.getGender());
+        System.out.println("House: " + character.getHouse());
+        System.out.println("Boggart: " + character.getBoggart());
+        System.out.println("Patronus: " + character.getPatronus() + "\n");
+    }
+}
