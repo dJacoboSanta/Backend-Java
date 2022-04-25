ffed078e01d26e468ce64cf4a301f0277c378bf4
dJacoboSanta <84555759+dJacoboSanta@users.noreply.github.com>
2022-04-24 19:30:18 -0500

    First practice
    This is the fisrt practice for backend java

---
+++ Practice_Harry/practicaHarry/Practice_Harry.java
@@ -0,0 +1,13 @@
+package practicaHarry;
+
+public class Practice_Harry {
+    public static void main(String[] args){
+        Show show = new Show();
+
+        show.showCharacter1();
+        show.showCharacter2();
+        show.showCharacter3();
+        show.showCharacter4();
+        show.showCharacter5();
+    }
+}
